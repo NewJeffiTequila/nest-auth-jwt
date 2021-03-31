f3b321677afb11ee9b68ab6718e74fc2e7e343c6
Jefferson Costa <48127640+NewJeffiTequila@users.noreply.github.com>
2021-03-31 03:15:45 -0300

    Add files via upload

---
+++ src/app.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+
+@Controller()
+export class AppController {
+
+  constructor(private authService: AuthService) {}
+
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
